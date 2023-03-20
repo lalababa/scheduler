9fd584260396ba25e9c6d75f37bddbe4bb730385
lalababa <lalababadidi@gmail.com>
2023-03-20 15:45:55 -0400

    modify back and transition bugs

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -9,6 +9,8 @@ export default function useVisualMode(initial) {
     // If "replace" is not true, this code updates the "history" array by adding "newMode" to the end
     if (!replace) {
       setHistory(prev => [...prev, newMode])
+    } else {
+      setHistory(prev => [...prev.slice(0,prev.length-1),mode])
     }
     // This code updates the "mode" state variable with the new mode
     setMode(newMode)
@@ -18,9 +20,9 @@ export default function useVisualMode(initial) {
   const back = () => {
     // If the history array only contains one mode, there's no previous mode to go back to, so this code returns
     if (history.length === 1) return;
-    // const newHistory = history.slice(0,history.length - 1)
+    const newHistory = history[history.length - 2]
     setHistory(prev => prev.slice(0,history.length - 1))
-    setMode(prev => prev[prev.length - 1])
+    setMode(newHistory)
   };
 
   
